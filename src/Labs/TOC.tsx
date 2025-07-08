5396d9eb579c75617768b9b9652809304d51d70b
Hudson Kass <kass.h@northeastern.edu>
2025-07-08 01:05:06 -0700

    Assignment 1

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -8,6 +8,10 @@ export default function TOC() {
       <li><Link to="/Labs/Lab2">Lab 2</Link></li>
       <li><Link to="/Labs/Lab3">Lab 3</Link></li>
       <li><Link to="/Kambaz">Kambaz</Link></li>
+      <li><a href="https://github.com/Hudica/kambaz-react-web-app" 
+           target="_blank">
+          GitHub
+        </a></li>
     </ul>
   );
 }
