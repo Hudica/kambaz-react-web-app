0fffcdb55b3fc76fc55bb568f430036c938ca224
Hudson Kass <kass.h@northeastern.edu>
2025-07-16 10:59:53 +0900

    Assignment 2

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,6 +7,7 @@ export default function Labs() {
   return (
     <div>
       <h1>Labs</h1>
+      <h2>Hudson Kass</h2>
       <TOC />
       <Routes>
         <Route path="/" element={<Navigate to="Lab1" />} />
